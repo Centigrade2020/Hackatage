61428e56c65236db37258aec86f79c97ff765c7c
Dharundds <dharundds@gmail.com>
2023-04-15 17:32:35 +0530

    auth completed

--- backend/hack_app/urls.py
+++ backend/hack_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path("",views.home,name='home'),
     path("test",views.get_db,name='test'),
     path("create_user",views.create_user,name='cus'),
+    path("login_user",views.login_user,name='lus'),
 ]
