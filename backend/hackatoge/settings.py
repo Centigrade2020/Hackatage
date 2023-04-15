129368798c249ada0994b7d7b8f4fb931b02e9a3
Dharundds <dharundds@gmail.com>
2023-04-15 11:28:59 +0530

    init backend

--- backend/hackatoge/settings.py
+++ backend/hackatoge/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hack_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:5173'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
